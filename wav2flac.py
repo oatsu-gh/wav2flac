4bd506ccc98aedd4ab15f5f49259b4f33898215b
CrazY <panchi.psp@gmail.com>
2019-11-25 01:47:22 +0900

    ごみ箱に送るときにもプログレスバーを表示するようにした。

--- wav2flac.py
+++ wav2flac.py
@@ -90,7 +90,7 @@ def main():
         print('WAVファイルをごみ箱へ送ります。')
         # 用済みのWAVファイルをゴミ箱に送る
         for wav_file in wav_files:
-            send2trash(wav_file)
+            send2trash(tqdm(wav_file))
         print('WAVファイルをゴミ箱に送りました。')
     else:
         print('WAVファイルとFLACファイルの数が一致しません。')
