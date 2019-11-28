5ea7f96a28eedfe96eae934145187cef2bd26232
CrazY <panchi.psp@gmail.com>
2019-11-28 19:11:20 +0900

    WAV→flacでflac.exeを使えるようにしました。

--- wav2flac.py
+++ wav2flac.py
@@ -1,18 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # coding: utf-8
 # 2019年11月21日作成開始
 """
 フォルダ構成を維持したままWAVファイルをFLACに変換する。
 完了後にWAVファイルはゴミ箱に送る。
 """
+import os
+import subprocess
 import sys
 from glob import glob
-from os.path import getsize
 
-# import ffmpeg
-from tqdm import tqdm
 from pydub import AudioSegment
 from send2trash import send2trash
+# import ffmpeg
+from tqdm import tqdm
+
 # from pysnooper import snoop
 
 
@@ -23,7 +25,7 @@ def glob_audiofile(dir_path, audio_format):
     # ファイルを再帰的に取得
     audio_files = glob('{}/*.{}'.format(dir_path, audio_format))
     # ファイルサイズを取得
-    audio_sizes = list(map(getsize, audio_files))
+    audio_sizes = list(map(os.path.getsize, audio_files))
     # 総データサイズを取得
     total_size = sum(audio_sizes)
     # ファイル一覧を返答
@@ -33,15 +35,26 @@ def glob_audiofile(dir_path, audio_format):
 def wav2flac(wav_files):
     """
     WAVファイルをFLACエンコード
-    この際トラック情報をコピーする(未実装)
+    ffmpeg.exeかflac.exeが必要
     """
-    for wav_file in tqdm(wav_files):
-        # ファイルを読み取り
-        f = AudioSegment.from_file(wav_file)
-        # flac用にファイル名を変更
-        flac_file = wav_file.replace('.wav', '.flac')
-        # flacで保存
-        f.export(flac_file, format='flac')
+    # flac.exeを利用
+    if os.path.isfile('./flac.exe'):
+        flac_arg = ' '.join(wav_files)
+        subprocess.call('.\\flac.exe -f {}'.format(flac_arg, shell=True))  # ファイルを変換（上書き）
+        subprocess.call('.\\flac.exe -t -F {}'.format(flac_arg, shell=True))  # 破損検査・修復
+
+        # ffmpegを利用
+    elif os.path.isfile('./ffmpeg.exe'):
+        for wav_file in tqdm(wav_files):
+            # ファイルを読み取り
+            f = AudioSegment.from_file(wav_file)
+            # flac用にファイル名を変更
+            flac_file = wav_file.replace('.wav', '.flac')
+            # flacで保存
+            f.export(flac_file, format='flac')
+
+    else:
+        print('ffmpeg.exeまたはflac.exeをwavfile.pyと同じ階層に用意してください。')
 
 
 def check_files(wav_files, flac_files):
@@ -53,6 +66,8 @@ def check_files(wav_files, flac_files):
     return False
 
 # @snoop()
+
+
 def main():
     """
     全体の処理を実行
